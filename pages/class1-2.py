08f9060e7245ae2c4a2aa812ad0ce3652ce4e474
tim0519 <timm312ro@gmail.com>
2024-08-12 16:51:08 +0800

    class1

---
+++ pages/class1-2.py
@@ -0,0 +1,32 @@
+import streamlit as st
+
+st.markdown(
+    """
+# 這是第一大標題
+
+- bbb
+- eee
+- www
+
+## 這是第二大標題
+
+```python
+print("hello world")
+```
+
+### 這是第三大標題
+
+---eeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+
+#### 這是第四大標題
+
+---
+
+##### 這是第五大標題
+
+wdwdwdwdwdwdwds
+
+###### 這是第六大標題
+
+"""
+)
