c212974da0d66675efebf2c88701d46316cda2b5
tim0519 <timm312ro@gmail.com>
2024-08-16 10:13:59 +0800

    8

--- pages/class5-1.py
+++ pages/class5-1.py
@@ -12,3 +12,49 @@ def calculate_square_area():
 length = 10
 calculate_square_area()
 # print("長度是",area) 這行會出錯,因為area是區域變數,只能在函數內部使用
+
+length = 5
+area = 100
+
+
+def calculate_square_area():
+    area = length**2
+
+
+calculate_square_area()
+print("面積是", area)
+
+length = 5
+area = 100
+
+
+def calculate_square_area():
+    area = length**2  # area是全域變數,length是全域變數
+    return area
+
+
+area = calculate_square_area()
+print("面積是", area)
+
+length = 5
+area = 100
+
+def calculate_square_area():
+    global area
+    area = length**2  
+
+calculate_square_area()
+print("面積是", area)
+
+def hello(name: str):
+    """
+    指令說明區\n
+    這是一個打招呼的函數\n
+    參數:\n
+    name: str - 姓名
+
+    回傳None
+
+    print(f"hello {name}!")
+
+
